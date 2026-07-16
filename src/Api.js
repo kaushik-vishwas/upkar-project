92b61af6f57a746788362c356ba088fe91bf427c
kaushik-vishwas <kaushikbiswasedu@gmail.com>
2026-07-16 13:44:38 +0530

    first commit

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_URL =
   // import.meta.env.VITE_API_URL || 'https://upkar-backend.onrender.com/api';
-  import.meta.env.VITE_API_URL || 'https://upkar.delicod.com/api';
+  import.meta.env.VITE_API_URL || 'https://backend.upkardevelopers.co.in';
 // import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 //Admin Auth
